4e00fe6e517db2003d735a661587e40c16730c2d
Doug Banks <doug.banks@gmail.com>
2025-08-27 12:44:15 -0700

    various style twiddles, some lua for TTS

--- TTSScripts/global.lua
+++ TTSScripts/global.lua
@@ -1,9 +1,21 @@
--- When a tiling card leaves the deck of tiling cards, turn snapping off for the card.
+-- When a card leaves the tiling deck, mark it as a tiling card.
 function onObjectLeaveContainer(container, object)
   if container.hasTag("tilingDeck") then
     -- This card should not snap.
     print("Card drawn: " .. object.getName())
     print("From deck: " .. container.getName())
-    object.setSnapPoints({})
+    object.addTag("tilingCard")
   end
+end
+
+function tryObjectEnterContainer(container, object)
+  print("object trying to enter container")
+  print("container.getName() = ", container.getName())
+  print("object.getName() = ", object.getName())
+  -- Tiling cards can never rejoin a deck.
+  if object.hasTag("tilingCard") then
+    print("Tiling card can't enter deck");
+    return false
+  end
+  return true
 end
